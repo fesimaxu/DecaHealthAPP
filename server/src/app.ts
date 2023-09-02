017cbd084f8eef7282890e680dde0e95d7212abe
Decagon <decagon@APPLEs-MacBook-Air.local>
2023-09-02 17:15:40 +0100

    update

---
+++ server/src/app.ts
@@ -0,0 +1,48 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import dotenv from "dotenv";
+import mongoose, { ConnectOptions } from "mongoose";
+import userRouter from "./routes/UserRouter";
+import doctorRouter from "./routes/DoctorRouter"
+import cors from 'cors';
+import appointmentRouter from "./routes/AppointmentRouter"
+
+dotenv.config();
+
+const app = express();
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+app.use('/user', userRouter);
+app.use('/doctor', doctorRouter);
+app.use('/', appointmentRouter);
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || '', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+} as ConnectOptions)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('MongoDB connection error:', error);
+  });
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`This application is listening at ${process.env.PORT}`)
+});
+
+
+
+
+export default app;
